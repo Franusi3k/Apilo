6e4fac5eebab3b7e084305eca1dab839805174d2
Dragonek <dragonekytl@gmail.com>
2025-10-14 13:13:19 +0200

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { ZiggyVue } from 'ziggy-js';
 
 createInertiaApp({
   resolve: name => import(`./Pages/${name}.vue`),
   setup({ el, app, props, plugin }) {
     createApp({ render: () => h(app, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .mount(el)
   },
 });
