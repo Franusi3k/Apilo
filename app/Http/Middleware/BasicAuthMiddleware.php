2402708a9640293347c1886919577615a8bf46df
Franusi3k <franekpaterka@op.pl>
2025-10-21 00:23:06 +0200

    Small login changes, password hashing

--- app/Http/Middleware/BasicAuthMiddleware.php
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -16,12 +16,12 @@ class BasicAuthMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $USERNAME = 'admin';
-        $PASSWORD = 'u7Z6il2kT$';
+        $PASSWORD = 'ceb5ca45c03499706aee2cfa28b239a1d497e6ff760c0283859add7d4514824c';
 
         $user = $request->getUser();
         $pass = $request->getPassword();
 
-        if ($user !== $USERNAME || $pass !== $PASSWORD) {
+        if ($user !== $USERNAME || !hash_equals($PASSWORD, hash("sha256", $pass))) {
             return response('Zaloguj się żeby korzystać ze strony', 401, [
                 'WWW-Authenticate' => 'Basic realm="Apilo Access"',
             ]);
