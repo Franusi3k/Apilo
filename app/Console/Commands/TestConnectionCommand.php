edaa4015a6ccd1ed9127e7a0f65b6b78ccbfb907
Franusi3k <franekpaterka@op.pl>
2025-12-08 17:27:44 +0100

    feat: command to check connection with Apilo API

---
+++ app/Console/Commands/TestConnectionCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Apilo\ApiloClient;
+use Illuminate\Console\Command;
+
+class TestConnectionCommand extends Command
+{
+    protected $signature = 'apilo:test-connection';
+    protected $description = 'Connection test with API Apilo';
+
+    public function __construct(private ApiloClient $client)
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $response = $this->client->get('/rest/api');
+
+        if (!$response->successful()) {
+            $this->error("Nie udało się nawiązać połączenia... " . $response->json('message'));
+            return Command::FAILURE;;
+        }
+
+        $this->info("Udało się nawiązać połączenie!");
+        return Command::SUCCESS;
+    }
+}
