12f0e4013268f8c023c1ebd3d242acc24d2ded70
Franusi3k <franekpaterka@op.pl>
2026-01-03 21:16:16 +0100

    fix: changes in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->web();
+        $middleware->api();
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
